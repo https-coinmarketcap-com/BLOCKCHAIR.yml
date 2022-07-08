f12ee4930f993931d1466b2029806ab7c4677caf
CoinMarketCap Australia NFT'S <108347651+CoinMarketCapAustraliaNFTs@users.noreply.github.com>
2022-07-08 17:28:35 +1000

    index.ts
    BLOCKCHAIR.yml
    /
    src
    /
    index.ts
    in
    master
     
    @@ -3,7 +3,119 @@ import { Collection, HookCollection } from "before-after-hook";
    import { request } from "@octokit/request";
    import { graphql, withCustomRequest } from "@octokit/graphql";
    import { createTokenAuth } from "@octokit/auth-token";
    core.js
    
    Extendable client for GitHub's REST & GraphQL APIs
    @latest Build Status
    Usage
    REST API example
    GraphQL example
    Options
    Defaults
    Authentication
    Logging
    Hooks
    Plugins
    Build your own Octokit with Plugins and Defaults
    LICENSE
    If you need a minimalistic library to utilize GitHub's REST API and GraphQL API which you can extend with plugins as needed, then @octokit/core is a great starting point.
    
    If you don't need the Plugin API then using @octokit/request or @octokit/graphql directly is a good alternative.
    Usage
    Browsers	Load @octokit/core directly from cdn.skypack.dev
    <script type="module">
    import { Octokit } from "https://cdn.skypack.dev/@octokit/core";
    </script>
    Node	
    Install with npm install @octokit/core
    const { Octokit } = require("@octokit/core");
    // or: import { Octokit } from "@octokit/core";
    REST API example
    // Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
    const octokit = new Octokit({ auth: `personal-access-token123` });
    const response = await octokit.request("GET /orgs/{org}/repos", {
      org: "octokit",
      type: "private",
    });
    See @octokit/request for full documentation of the .request method.
    GraphQL example
    const octokit = new Octokit({ auth: `secret123` });
    const response = await octokit.graphql(
      `query ($login: String!) {
        organization(login: $login) {
          repositories(privacy: PRIVATE) {
            totalCount
          }
        }
      }`,
      { login: "octokit" }
    );
    See @octokit/graphql for full documentation of the .graphql method.
    Options
    name	type	description
    options.authStrategy	Function	Defaults to @octokit/auth-token. See Authentication below for examples.
    options.auth	String or Object	See Authentication below for examples.
    options.baseUrl	String	
    When using with GitHub Enterprise Server, set options.baseUrl to the root URL of the API. For example, if your GitHub Enterprise Server's hostname is github.acme-inc.com, then set options.baseUrl to https://github.acme-inc.com/api/v3. Example
    
    const octokit = new Octokit({
    
      baseUrl: "https://github.acme-inc.com/api/v3",
    
    });
    
    options.previews	
    
    BLOCKCHAIR.yml
    import {
      Constructor,
      Hooks,
    @CoinMarketCapAustraliaNFTs
    Commit changes.

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,119 @@ import { Collection, HookCollection } from "before-after-hook";
 import { request } from "@octokit/request";
 import { graphql, withCustomRequest } from "@octokit/graphql";
 import { createTokenAuth } from "@octokit/auth-token";
+core.js
 
+Extendable client for GitHub's REST & GraphQL APIs
+
+@latest Build Status
+
+Usage
+
+REST API example
+
+GraphQL example
+
+Options
+
+Defaults
+
+Authentication
+
+Logging
+
+Hooks
+
+Plugins
+
+Build your own Octokit with Plugins and Defaults
+
+LICENSE
+
+If you need a minimalistic library to utilize GitHub's REST API and GraphQL API which you can extend with plugins as needed, then @octokit/core is a great starting point.
+
+If you don't need the Plugin API then using @octokit/request or @octokit/graphql directly is a good alternative.
+
+Usage
+
+Browsers	Load @octokit/core directly from cdn.skypack.dev
+
+<script type="module">
+
+import { Octokit } from "https://cdn.skypack.dev/@octokit/core";
+
+</script>
+
+Node	
+
+Install with npm install @octokit/core
+
+const { Octokit } = require("@octokit/core");
+
+// or: import { Octokit } from "@octokit/core";
+
+REST API example
+
+// Create a personal access token at https://github.com/settings/tokens/new?scopes=repo
+
+const octokit = new Octokit({ auth: `personal-access-token123` });
+
+const response = await octokit.request("GET /orgs/{org}/repos", {
+
+  org: "octokit",
+
+  type: "private",
+
+});
+
+See @octokit/request for full documentation of the .request method.
+
+GraphQL example
+
+const octokit = new Octokit({ auth: `secret123` });
+
+const response = await octokit.graphql(
+
+  `query ($login: String!) {
+
+    organization(login: $login) {
+
+      repositories(privacy: PRIVATE) {
+
+        totalCount
+
+      }
+
+    }
+
+  }`,
+
+  { login: "octokit" }
+
+);
+
+See @octokit/graphql for full documentation of the .graphql method.
+
+Options
+
+name	type	description
+
+options.authStrategy	Function	Defaults to @octokit/auth-token. See Authentication below for examples.
+
+options.auth	String or Object	See Authentication below for examples.
+
+options.baseUrl	String	
+
+When using with GitHub Enterprise Server, set options.baseUrl to the root URL of the API. For example, if your GitHub Enterprise Server's hostname is github.acme-inc.com, then set options.baseUrl to https://github.acme-inc.com/api/v3. Example
+
+const octokit = new Octokit({
+
+  baseUrl: "https://github.acme-inc.com/api/v3",
+
+});
+
+options.previews	
+
+BLOCKCHAIR.yml
 import {
   Constructor,
   Hooks,
